045d51eb245b04c181e5d43e54c8f6f9c85bf63c
Underemployed <underemployed871@gmail.com>
2023-08-29 14:09:57 +0530

    faster api

--- api/app.py
+++ api/app.py
@@ -1,31 +1,27 @@
-import requests
+import asyncio
+import tldextract  
+from flask import Flask, jsonify, request
 from urllib.request import Request, urlopen
 from urllib.parse import quote
 from bs4 import BeautifulSoup
 import newspaper
 import json
-import os
-from flask import Flask, jsonify,request
 from flask_cors import CORS
 import nltk
+from concurrent.futures import ThreadPoolExecutor
+
 nltk.download('punkt')
+
 app = Flask(__name__)
 CORS(app)
+
 class ArticleScraper:
-    def __init__(self, query, max_retries=3, blacklist_file="blacklist.txt", data_file="articles.json"):
+    def __init__(self, query, max_retries=3):
         self.query = query
         self.max_retries = max_retries
-        self.blacklist_file = blacklist_file
-        self.data_file = data_file
         self.query_encoded = quote(query)
         self.articles_data = []
-
-    def load_blacklist(self):
-        try:
-            with open(self.blacklist_file, 'r') as file:
-                return set(file.read().splitlines())
-        except FileNotFoundError:
-            return set()
+        self.blacklist = set()  # Initialize an empty blacklist
 
     def download_article_with_retry(self, url):
         retries = 0
@@ -38,68 +34,88 @@ class ArticleScraper:
                 return article
             except Exception as e:
                 print(f"Error downloading article from {url}: {str(e)}")
-                print(f"Retrying in 5 seconds...")
                 retries += 1
                 continue
+        # If all retries fail, add the domain to the blacklist and return None
+        domain = url
+        print(f"Adding {domain} to the blacklist due to repeated failures.")
+        self.add_to_blacklist(domain)
         return None
 
-    def fetch_articles(self):
+    async def fetch_articles(self):
         link = f"https://www.google.co.in/search?q={self.query_encoded}&num=1&gl=IN&tbs=sbd:1,qdr:d&tbm=nws&source=lnt&num=100&gl=IN"
         print(link)
         req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
         webpage = urlopen(req).read()
-        blacklist = self.load_blacklist()
 
-        with requests.Session() as c:
-            soup = BeautifulSoup(webpage, 'lxml')
-            url_tags = soup.find_all('a')
-            for tag in url_tags:
-                url = tag.get('href', '')
-                if '/url?q=' in url:
-                    modified_url = url.split('/url?q=')[1].split('&sa=')[0]
-                    domain = modified_url.split('/')[2].lower()
-                    if domain in blacklist:
-                        print(f"Skipping blacklisted domain: {domain}")
-                        continue
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            loop = asyncio.get_event_loop()
+            futures = [loop.run_in_executor(executor, self.parse_url, url) for url in self.get_urls(webpage)]
+            for response in await asyncio.gather(*futures):
+                if response:
+                    self.articles_data.append(response)
 
-                    article = self.download_article_with_retry(modified_url)
-                    if article is None:
-                        print(f"Skipping article: {modified_url}")
-                        blacklist.add(domain)
-                        continue
+    def get_urls(self, webpage):
+        soup = BeautifulSoup(webpage, 'lxml')
+        url_tags = soup.find_all('a')
+        for tag in url_tags:
+            url = tag.get('href', '')
+            if '/url?q=' in url:
+                modified_url = url.split('/url?q=')[1].split('&sa=')[0]
+                yield modified_url
 
-                    title = article.title
-                    if 'you a robot?' in title.lower():
-                        continue
-                    summary = article.summary
-                    source = modified_url
-                    publish_time = article.publish_date.strftime("%Y-%m-%d %H:%M:%S") if article.publish_date else ""
+    def parse_url(self, url):
+        # Check if the domain contains "google" (case-insensitive) and add it to the blacklist
+        if 'google' in url.lower():
+            print(f"Skipping blacklisted domain: {url}")
+            domain = tldextract.extract(url).domain  # Extract the domain using tldextract
+            self.add_to_blacklist(domain.lower())
+            return None
 
-                    if len(summary.split()) < 30:
-                        continue
-                    
-                    data = {
-                        "title": title,
-                        "publish_time": publish_time,
-                        "summary": summary,
-                        "source": source
-                    }
-                    self.articles_data.append(data)
+        article = self.download_article_with_retry(url)
+        if article is None:
+            print(f"Skipping article: {url}")
+            return None
 
-        with open(self.blacklist_file, 'w') as file:
-            file.write('\n'.join(blacklist))
+        title = article.title
+        if 'you a robot?' in title.lower():
+            return None
+        summary = article.summary
+        source = url
+        publish_time = article.publish_date.strftime("%Y-%m-%d %H:%M:%S") if article.publish_date else ""
 
-        with open(self.data_file, 'w', encoding='utf-8') as json_file:
-            json.dump(self.articles_data, json_file, ensure_ascii=False, indent=4)
+        if len(summary.split()) < 30:
+            return None
+
+        data = {
+            "title": title,
+            "publish_time": publish_time,
+            "summary": summary,
+            "source": source
+        }
+        print(title)
+        return data
+
+
+    # Method to add a domain to the blacklist
+    # Modify the add_to_blacklist method
+    def add_to_blacklist(self, url):
+        domain = tldextract.extract(url).domain  # Extract the domain using tldextract
+        self.blacklist.add(domain.lower())
+        print(f"Adding {domain} to the blacklist")
 
 
 @app.route('/api/articles', methods=['GET'])
-def scrape_articles():
+async def scrape_articles():
     query = request.args.get('query', 'indian stock')  # Default to "indian stock" if query parameter is not provided
     scraper = ArticleScraper(query)
-    scraper.fetch_articles()
-    with open(scraper.data_file, 'r', encoding='utf-8') as json_file:
-        articles_data = json.load(json_file)
-    return jsonify({"articles": articles_data})
+    await scraper.fetch_articles()
+    print("Completed")
+
+    # Print blacklisted websites
+    print("Blacklisted Websites:", ', '.join(scraper.blacklist))
+
+    return jsonify({"articles": scraper.articles_data})
+
 if __name__ == '__main__':
     app.run(debug=True)
